172a7cb3b03370288271d65196c996ac99e29a5e
Кирилл <kirainluck@gmail.com>
2020-05-29 23:16:34 +0300

    formatted sql

--- 15/OLAP_insert.sql
+++ 15/OLAP_insert.sql
@@ -1,106 +1,293 @@
-insert into sales_date (ddate)
-select distinct ddate from recept;
-
-insert into purchase_date (ddate)
-select distinct ddate from income;
-
-insert into remains_date (ddate)
-select distinct ddate from remains;
-
-insert into sales_storage (id, name, active)
-select id, name, active from storages where id in
-(select distinct storage from recept);
-
-insert into purchase_storage (id, name, active)
-select id, name, active from storages where id in
-(select distinct storage from income);
-
-insert into remains_storage (id, name, active)
-select id, name, active from storages where id in
-(select distinct storage from remains);
-
-insert into sales_goods(id, name, g_group, weight, length, height, width, vol)
-select id, name, g_group, weight, length, height, width, length*height*width 
-from goods where id in (select distinct goods from recgoods);
-
-insert into purchase_goods(id, name, g_group, weight, length, height, width, vol)
-select id, name, g_group, weight, length, height, width, length*height*width 
-from goods where id in (select distinct goods from incgoods);
-
-insert into remains_goods(id, name, g_group, weight, length, height, width, vol)
-select id, name, g_group, weight, length, height, width, length*height*width 
-from goods where id in (select distinct goods from remains);
-
-insert into sales_clients(id, name, address, city, client_groups, region)
-select clients.id, clients.name, clients.address, clients.city,
-clients.client_groups, city.region from clients
-join city on city.id = clients.city
-where clients.id in (select distinct client from recept);
-
-insert into purchase_clients(id, name, address, city, client_groups, region)
-select clients.id, clients.name, clients.address, clients.city,
-clients.client_groups, city.region from clients
-join city on city.id = clients.city
-where clients.id in (select distinct client from income);
-
-
-insert into purchase_fact_table(amount, quantity, volume,
-							   weight, ddate_id, client_id, storage_id, goods_id)
-select incgoods.volume*incgoods.price as amount,
-incgoods.volume as quantity,
-goods.height*goods.width*goods.length*incgoods.volume as volume,
-goods.weight*incgoods.volume as weight,
-purchase_date.id, purchase_clients.id, purchase_storage.id, purchase_goods.id
-from incgoods
-join income on income.id = incgoods.id
-join goods on goods.id = incgoods.goods
-join purchase_date on purchase_date.ddate = income.ddate
-join purchase_clients on purchase_clients.id = income.client
-join purchase_storage on purchase_storage.id = income.storage
-join purchase_goods on purchase_goods.id = incgoods.goods;
-
-
-insert into remains_fact_table(amount, quantity, volume,
-							   weight, ddate_id, storage_id, goods_id)
-
-select incgoods.price*rem_vols.vol as amount, rem_vols.vol as count,
-goods.height*goods.width*goods.length*rem_vols.vol as volume,
-goods.weight*rem_vols.vol as weight,
-remains_date.id, remains_storage.id, remains_goods.id
-from remains join goods on goods.id = remains.goods
-join 
-(
-select rem.id, rem.subid, rem.goods, (rem.vol-irlink.volume) as vol from
-(select id, subid, goods, sum(volume) as vol from remains
-group by id, subid, goods) rem
-join irlink on (irlink.i_id = rem.id and irlink.i_subid = rem.subid 
-				and irlink.goods = rem.goods)) rem_vols
-on (remains.id = rem_vols.id and remains.subid = rem_vols.subid
-   and remains.goods = rem_vols.goods)
-join incgoods on (incgoods.id = rem_vols.id and incgoods.subid = rem_vols.subid
-				 and incgoods.goods = rem_vols.goods)
-join remains_date on remains_date.ddate = remains.ddate
-join remains_storage on remains_storage.id = remains.storage
-join remains_goods on remains_goods.id = remains.goods;
-
-insert into sales_fact_table(amount, quantity, volume,
-							   weight, cost_price, ddate_id, client_id, storage_id, goods_id)
-select recgoods.volume*recgoods.price as amount,
-recgoods.volume as quantity,
-goods.height*goods.width*goods.length*recgoods.volume as volume,
-goods.weight*recgoods.volume as weight, cst.cost_price,
-sales_date.id, sales_clients.id, sales_storage.id, sales_goods.id
-from recgoods
-join recept on recept.id = recgoods.id
-join goods on goods.id = recgoods.goods
-join
-(
-select goods, sum(price)/sum(volume)::real as cost_price from incgoods
-group by goods) cst
-on cst.goods = recgoods.goods
-join sales_date on sales_date.ddate = recept.ddate
-join sales_clients on sales_clients.id = recept.client
-join sales_storage on sales_storage.id = recept.storage
-join sales_goods on sales_goods.id = recgoods.goods;
-
-
+INSERT INTO sales_date (ddate) 
+SELECT 
+  DISTINCT ddate 
+FROM 
+  recept;
+INSERT INTO purchase_date (ddate) 
+SELECT 
+  DISTINCT ddate 
+FROM 
+  income;
+INSERT INTO remains_date (ddate) 
+SELECT 
+  DISTINCT ddate 
+FROM 
+  remains;
+  
+  
+INSERT INTO sales_storage (id, name, active) 
+SELECT 
+  id, 
+  name, 
+  active 
+FROM 
+  storages 
+WHERE 
+  id IN (
+    SELECT 
+      DISTINCT STORAGE 
+    FROM 
+      recept
+  );
+  
+  
+INSERT INTO purchase_storage (id, name, active) 
+SELECT 
+  id, 
+  name, 
+  active 
+FROM 
+  storages 
+WHERE 
+  id IN (
+    SELECT 
+      DISTINCT STORAGE 
+    FROM 
+      income
+  );
+  
+  
+INSERT INTO remains_storage (id, name, active) 
+SELECT 
+  id, 
+  name, 
+  active 
+FROM 
+  storages 
+WHERE 
+  id IN (
+    SELECT 
+      DISTINCT STORAGE 
+    FROM 
+      remains
+  );
+  
+  
+INSERT INTO sales_goods(
+  id, name, g_group, weight, LENGTH, height, 
+  width, vol
+) 
+SELECT 
+  id, 
+  name, 
+  g_group, 
+  weight, 
+  LENGTH, 
+  height, 
+  width, 
+  LENGTH * height * width 
+FROM 
+  goods 
+WHERE 
+  id IN (
+    SELECT 
+      DISTINCT goods 
+    FROM 
+      recgoods
+  );
+  
+  
+INSERT INTO purchase_goods(
+  id, name, g_group, weight, LENGTH, height, 
+  width, vol
+) 
+SELECT 
+  id, 
+  name, 
+  g_group, 
+  weight, 
+  LENGTH, 
+  height, 
+  width, 
+  LENGTH * height * width 
+FROM 
+  goods 
+WHERE 
+  id IN (
+    SELECT 
+      DISTINCT goods 
+    FROM 
+      incgoods
+  );
+  
+  
+INSERT INTO remains_goods(
+  id, name, g_group, weight, LENGTH, height, 
+  width, vol
+) 
+SELECT 
+  id, 
+  name, 
+  g_group, 
+  weight, 
+  LENGTH, 
+  height, 
+  width, 
+  LENGTH * height * width 
+FROM 
+  goods 
+WHERE 
+  id IN (
+    SELECT 
+      DISTINCT goods 
+    FROM 
+      remains
+  );
+  
+  
+INSERT INTO sales_clients(
+  id, name, address, city, client_groups, 
+  region
+) 
+SELECT 
+  clients.id, 
+  clients.name, 
+  clients.address, 
+  clients.city, 
+  clients.client_groups, 
+  city.region 
+FROM 
+  clients 
+  JOIN city ON city.id = clients.city 
+WHERE 
+  clients.id IN (
+    SELECT 
+      DISTINCT client 
+    FROM 
+      recept
+  );
+  
+  
+INSERT INTO purchase_clients(
+  id, name, address, city, client_groups, 
+  region
+) 
+SELECT 
+  clients.id, 
+  clients.name, 
+  clients.address, 
+  clients.city, 
+  clients.client_groups, 
+  city.region 
+FROM 
+  clients 
+  JOIN city ON city.id = clients.city 
+WHERE 
+  clients.id IN (
+    SELECT 
+      DISTINCT client 
+    FROM 
+      income
+  );
+  
+  
+INSERT INTO purchase_fact_table(
+  amount, quantity, volume, weight, ddate_id, 
+  client_id, storage_id, goods_id
+) 
+SELECT 
+  incgoods.volume * incgoods.price AS amount, 
+  incgoods.volume AS quantity, 
+  goods.height * goods.width * goods.length * incgoods.volume AS volume, 
+  goods.weight * incgoods.volume AS weight, 
+  purchase_date.id, 
+  purchase_clients.id, 
+  purchase_storage.id, 
+  purchase_goods.id 
+FROM 
+  incgoods 
+  JOIN income ON income.id = incgoods.id 
+  JOIN goods ON goods.id = incgoods.goods 
+  JOIN purchase_date ON purchase_date.ddate = income.ddate 
+  JOIN purchase_clients ON purchase_clients.id = income.client 
+  JOIN purchase_storage ON purchase_storage.id = income.storage 
+  JOIN purchase_goods ON purchase_goods.id = incgoods.goods;
+  
+  
+INSERT INTO remains_fact_table(
+  amount, quantity, volume, weight, ddate_id, 
+  storage_id, goods_id
+) 
+SELECT 
+  incgoods.price * rem_vols.vol AS amount, 
+  rem_vols.vol AS COUNT, 
+  goods.height * goods.width * goods.length * rem_vols.vol AS volume, 
+  goods.weight * rem_vols.vol AS weight, 
+  remains_date.id, 
+  remains_storage.id, 
+  remains_goods.id 
+FROM 
+  remains 
+  JOIN goods ON goods.id = remains.goods 
+  JOIN (
+    SELECT 
+      rem.id, 
+      rem.subid, 
+      rem.goods, 
+      (rem.vol - irlink.volume) AS vol 
+    FROM 
+      (
+        SELECT 
+          id, 
+          subid, 
+          goods, 
+          sum(volume) AS vol 
+        FROM 
+          remains 
+        GROUP BY 
+          id, 
+          subid, 
+          goods
+      ) rem 
+      JOIN irlink ON (
+        irlink.i_id = rem.id 
+        AND irlink.i_subid = rem.subid 
+        AND irlink.goods = rem.goods
+      )
+  ) rem_vols ON (
+    remains.id = rem_vols.id 
+    AND remains.subid = rem_vols.subid 
+    AND remains.goods = rem_vols.goods
+  ) 
+  JOIN incgoods ON (
+    incgoods.id = rem_vols.id 
+    AND incgoods.subid = rem_vols.subid 
+    AND incgoods.goods = rem_vols.goods
+  ) 
+  JOIN remains_date ON remains_date.ddate = remains.ddate 
+  JOIN remains_storage ON remains_storage.id = remains.storage 
+  JOIN remains_goods ON remains_goods.id = remains.goods;
+  
+  
+INSERT INTO sales_fact_table(
+  amount, quantity, volume, weight, cost_price, 
+  ddate_id, client_id, storage_id, 
+  goods_id
+) 
+SELECT 
+  recgoods.volume * recgoods.price AS amount, 
+  recgoods.volume AS quantity, 
+  goods.height * goods.width * goods.length * recgoods.volume AS volume, 
+  goods.weight * recgoods.volume AS weight, 
+  cst.cost_price, 
+  sales_date.id, 
+  sales_clients.id, 
+  sales_storage.id, 
+  sales_goods.id 
+FROM 
+  recgoods 
+  JOIN recept ON recept.id = recgoods.id 
+  JOIN goods ON goods.id = recgoods.goods 
+  JOIN (
+    SELECT 
+      goods, 
+      sum(price)/ sum(volume):: real AS cost_price 
+    FROM 
+      incgoods 
+    GROUP BY 
+      goods
+  ) cst ON cst.goods = recgoods.goods 
+  JOIN sales_date ON sales_date.ddate = recept.ddate 
+  JOIN sales_clients ON sales_clients.id = recept.client 
+  JOIN sales_storage ON sales_storage.id = recept.storage 
+  JOIN sales_goods ON sales_goods.id = recgoods.goods;
